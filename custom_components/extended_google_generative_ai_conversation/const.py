fe1f8acc6b81a1150086ad66060bdd5bb9075688
Tianzhi <58951390+tianzhihe@users.noreply.github.com>
2025-03-12 17:30:21 -0400

    Update const.py

--- custom_components/extended_google_generative_ai_conversation/const.py
+++ custom_components/extended_google_generative_ai_conversation/const.py
@@ -30,4 +30,53 @@ CONF_SEXUAL_BLOCK_THRESHOLD = "sexual_block_threshold"
 CONF_DANGEROUS_BLOCK_THRESHOLD = "dangerous_block_threshold"
 RECOMMENDED_HARM_BLOCK_THRESHOLD = "BLOCK_MEDIUM_AND_ABOVE"
 
+CONF_FUNCTIONS = "functions"
+DEFAULT_CONF_FUNCTIONS = [
+    {
+        "spec": {
+            "name": "execute_services",
+            "description": "Use this function to execute service of devices in Home Assistant.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "list": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "domain": {
+                                    "type": "string",
+                                    "description": "The domain of the service",
+                                },
+                                "service": {
+                                    "type": "string",
+                                    "description": "The service to be called",
+                                },
+                                "service_data": {
+                                    "type": "object",
+                                    "description": "The service data object to indicate what to control.",
+                                    "properties": {
+                                        "entity_id": {
+                                            "type": "string",
+                                            "description": "The entity_id retrieved from available devices. It must start with domain, followed by dot character.",
+                                        }
+                                    },
+                                    "required": ["entity_id"],
+                                },
+                            },
+                            "required": ["domain", "service", "service_data"],
+                        },
+                    }
+                },
+            },
+        },
+        "function": {"type": "native", "name": "execute_service"},
+    }
+]
+
+CONF_USE_TOOLS = "use_tools"
+
+
+
+
 TIMEOUT_MILLIS = 10000
